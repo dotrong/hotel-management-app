6825db67e5f0c47aa6f6cafc90b4c38c9736f686
brianramaswami <brianramaswami@yahoo.com>
2017-07-11 21:01:47 -0700

    updated css

---
+++ public/js/login.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+$("#login_button").click(function() {
+alert("hello world");
+});
+
+$("#newUser_button").click(function() {
+
+});
+
+
+$("#login_button").click(function() {
+
+});
+
+});
