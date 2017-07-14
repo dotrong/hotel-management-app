9622d0a8da5fd1ebd741150bf227bb50248720aa
dotrong <dohieu@gmail.com>
2017-07-14 16:26:54 -0700

    add registration

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -14,29 +14,50 @@ module.exports = function(app) {
 
   // index route loads view.html
   app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
+    console.log(req);
+    if (req.session.userRole === 'customer') {
+      res.redirect("/customer");
+
+    }
+    else if (req.session.userRole == 'manager') {
+      res.redirect("/manager");
+    }
+    else {
+      console.log("no user info");
+      res.sendFile(path.join(__dirname, "../public/index.html"));
+
+    }
+    
   });
 
   // Login route
-  app.get("/login", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/login.html"));
-  });
+  // app.get("/login", function(req, res) {
+  //   res.sendFile(path.join(__dirname, "../public/login.html"));
+  // });
 
   // Selection route
-  app.get("/selection", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/selectroom.html"));
-  });
+  // app.get("/selection", function(req, res) {
+  //   res.sendFile(path.join(__dirname, "../public/selectroom.html"));
+  // });
 
-  // Form route
+  // manager will see everything in this hotel: rooms, guests, reservations...
   app.get("/manager", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/form.html"));
+    res.sendFile(path.join(__dirname, "../public/manager.html"));
   });
 
-  // Contact route
+  // customer route, will see reservation/make new reservation
   app.get("/customer", function(req, res) {
  
-     res.sendFile(path.join(__dirname, "../public/contact.html"));
+     res.sendFile(path.join(__dirname, "../public/customer.html"));
 
   });
+
+    app.get("/registration", function(req, res) {
+ 
+     res.sendFile(path.join(__dirname, "../public/registration.html"));
+
+  });
+
+
 
 };
