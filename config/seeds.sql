f769144f52670f4143ec661cd3574067e2854f8a
brianramaswami <brianramaswami@yahoo.com>
2017-07-13 21:21:17 -0700

    seeded sql

---
+++ config/seeds.sql
@@ -0,0 +1,21 @@
+use hotel;
+
+insert into users(username, password, user_role)
+values("bob", "abc", "customer");
+
+
+insert into guests(first_name, last_name)
+values("bob", "Robberts");
+
+insert into guests(first_name, last_name)
+values("Star", "Fox");
+
+insert into rooms(room_number, room_type, room_price)
+values(1, "Suite", 100.00);
+
+insert into rooms(room_number, room_type, room_price)
+values(2, "1 Bed", 75.00);
+
+
+
+select * from users;
