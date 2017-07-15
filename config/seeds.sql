53fed493db876cad0b80d645f05e8d5cd0a0d518
brianramaswami <brianramaswami@yahoo.com>
2017-07-14 18:56:07 -0700

    updated seeds file

--- config/seeds.sql
+++ config/seeds.sql
@@ -9,12 +9,53 @@ values("bob", "Robberts");
 insert into guests(first_name, last_name)
 values("Star", "Fox");
 
+#--item 1--
 insert into rooms(room_number, room_type, room_price)
-values(1, "Suite", 100.00);
+values(1, "Suite", 200.00);
 
+#--item 2--
 insert into rooms(room_number, room_type, room_price)
-values(2, "1 Bed", 75.00);
+values(2, "1 Bed", 100.00);
 
+#--item 3--
+insert into rooms(room_number, room_type, room_price)
+values(3, "2 Bed", 150.00);
+
+#--item 4--
+insert into rooms(room_number, room_type, room_price)
+values(4, "Suite", 200.00);
+
+#--item 5--
+insert into rooms(room_number, room_type, room_price)
+values(5, "1 Bed", 100.00);
+
+#--item 6--
+insert into rooms(room_number, room_type, room_price)
+values(6, "2 Bed", 150.00);
+
+#--item 7--
+insert into rooms(room_number, room_type, room_price)
+values(7, "Suite", 200.00);
+
+#--item 8--
+insert into rooms(room_number, room_type, room_price)
+values(8, "1 Bed", 100.00);
+
+#--item 9--
+insert into rooms(room_number, room_type, room_price)
+values(9, "2 Bed", 150.00);
+
+#--item 10--
+insert into rooms(room_number, room_type, room_price)
+values(10, "Suite", 200.00);
+
+#--item 11--
+insert into rooms(room_number, room_type, room_price)
+values(11, "1 Bed", 100.00);
+
+#--item 12--
+insert into rooms(room_number, room_type, room_price)
+values(12, "2 Bed", 150.00);
 
 
 select * from users;
